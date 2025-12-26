25455fb7c9a00278989b97b75d67839b4772306b
Zubair Ahmed <STr1k3R768@gmail.com>
2025-12-26 17:12:31 +0500

    Removed '/sign-in' and '/sign-up' routes from App.jsx Created Dashboard.jsx screen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route} from "react-router";
 import Header from "./components/Header";
 import Home from './screens/Home';
-import SignIn from "./screens/SignIn";
-import SignUp from "./screens/SignUp";
 import UserAuth from "./screens/UserAuth";
 // import ThemeController from "./components/ThemeController";
 
@@ -15,14 +13,6 @@ export default function App() {
           <Route
             path="/"
             element={<Home/>}
-          />
-          <Route
-            path="/sign-in"
-            element={<SignIn/>}
-          />
-          <Route
-            path="/sign-up"
-            element={<SignUp/>}
           />
           <Route
             path="/user-auth"
---
+++ src/screens/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Stat from "../components/Stat";
+
+export default function Home() {
+    return (
+        <section className="w-full max-w-300 mx-auto p-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 mb-10">
+                <Stat name={'Total Expense'} value={'20,00'}/>
+                <Stat name={'Current Month Expense'} value={'20,00'}/>
+                <Stat name={'Last Month Expense'} value={'20,00'}/>
+                <Stat name={'Income'} value={'20,00'}/>
+            </div>
+        </section>
+    )
+}
