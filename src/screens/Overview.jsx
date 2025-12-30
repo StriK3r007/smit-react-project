16cf627da1e5a68a01d21a30b99364cd1cc5ee30
Zubair Ahmed <STr1k3R768@gmail.com>
2025-12-30 19:48:30 +0500

    feat(overview): display expense statistics
    - Add responsive grid layout for stats
    - Include Stat components for Total Expense, Current Month Expenses, and Latest Expense
    - Update heading size and section layout

--- src/screens/Overview.jsx
+++ src/screens/Overview.jsx
@@ -1,3 +1,18 @@
+import Stat from '../components/Stat'
+
 export default function Overview() {
-    return <h1 className="text-xl font-bold">Overview</h1>;
+    return (
+        <section className="full min-h-screen">
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl font-bold">Overview</h1>
+            </div>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-4 mt-12'>
+                <Stat name="Total Expense" value="500" />
+                <Stat name="Current Month Expenses" value="500" />
+                <Stat name="Latest Expense" value="230" />
+                <Stat />
+                <Stat />
+            </div>
+        </section>
+    )
 }
